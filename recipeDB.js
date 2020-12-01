181433e43643b5b304da26d11fa004fcb99766db
kschieferecke <kschieferecke@gmail.com>
2020-12-01 15:25:59 -0600

    ryan and katie added comments
    commitment.

--- recipeDB.js
+++ recipeDB.js
@@ -9,6 +9,9 @@ db.once('open', function(){
     console.log("We're connected");
 
     /* ingredient schema (schema inside schema) */
+    /* this schema goes first becuase we need to declare the schemas that go inside of schemas first
+        this is because it is read from the top of the code to the bottom, line by line. it is 
+        not read all at once.  */
     const ingredientSchema = new mongoose.Schema({
         name : String,
         amount : String, 
@@ -19,6 +22,8 @@ db.once('open', function(){
 
 
     /* recipe schema */
+    //this schema references the ingredient schema
+    //the ingredient property is an array of Ingredient objects
     const recipeSchema = new mongoose.Schema({
         title : String, 
         ingredients : [ingredientSchema], /* a schema inside a schema */
@@ -34,6 +39,8 @@ db.once('open', function(){
     const Recipe = mongoose.model('Recipe', recipeSchema);
 
      /* saved recipe schema */
+     //SavedRecipe references a Recipe by ID
+     //hopefully this works
     const savedRecipeSchema = new mongoose.Schema({
         recipe_id: {
             type: mongoose.Schema.Types.ObjectId,
@@ -45,6 +52,7 @@ db.once('open', function(){
     const SavedRecipe = mongoose.model('SavedRecipe', savedRecipeSchema);
        
     /* user profile schema */
+    //saved_recipes property is an array of SavedRecipe objects
     const userProfileSchema = new mongoose.Schema ({
         username : String, 
         password : String, 
@@ -57,3 +65,19 @@ db.once('open', function(){
     const UserProfile = mongoose.model('UserProfile', userProfileSchema);
 
 }); //once
+
+/* 
+    KS: if we want to include charts, graphs, images, or other large objects, we use XML
+    with JSON we ONLY use data types. 
+        the main reason we are choosing json over xml is that JSON uses multiple different formats of data: 
+        we're only going to use strings, numbers, and sometimes boolean.
+    you don't want to mix XML and JSON (derulo)  
+    with this type of database, we are only able to format in BSON */
+
+/*  
+
+    RT: Another benefit of using JSON over XML when creating data schemas is the fleixibility that the JSON format provides.
+    JSON format allows us to handle data uncertainty as it gives us the ability to add/remove properties on the fly, instead
+        of being limited to what we defined at schema creation.
+    /mic drop
+*/
