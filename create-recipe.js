d8e9eba044423e2bfda8fc0e68691652f9e700a5
kschieferecke <kschieferecke@gmail.com>
2021-01-13 10:07:16 -0600

    page connection
    I added page connections

--- create-recipe.js
+++ create-recipe.js
@@ -1,3 +1,3 @@
 function addIngredient() {
-    document.getElementById("add").innerHTML = "<input type="text" id="ingredient" name="ingredient" placeholder="1 cup of strawberries"> <button type="submit">+</button><br>";
+    document.getElementById("add").innerHTML += '<input type="text" id="ingredient" name="ingredient" placeholder="1 cup of strawberries"><br>';
   }
