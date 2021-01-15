25b430c766ca8db1dee5dbdc3a3b75d9bf0eedef
rtanner0150 <71511920+tannertronic@users.noreply.github.com>
2021-01-15 10:34:59 -0600

    finish building search functionality

--- public/js/data.js
+++ public/js/data.js
@@ -25,3 +25,31 @@ async function getSpecificUser(id){
     }
     return body;
 }
+
+async function searchRecipesByTitle(query){
+    let requestOptions = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+    }
+    
+    const response = await fetch('/recipes/search_title/' + query, requestOptions);
+    const body = await response.json();
+    if (response.status != 200){
+        throw Error(body.message);
+    }
+    return body;
+}
+
+async function searchRecipesByIngredient(query){
+    let requestOptions = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+    }
+    
+    const response = await fetch('/recipes/search_ingredient/' + query, requestOptions);
+    const body = await response.json();
+    if (response.status != 200){
+        throw Error(body.message);
+    }
+    return body;
+}
--- public/js/search.js
+++ public/js/search.js
@@ -1,21 +1,45 @@
 document.getElementById('search-submit').addEventListener('click', () => {
-    getRecipes().then((recipes) => {
-        recipes.forEach(recipe => {
-            getSpecificUser(recipe.created_by).then((user) => {
-                document.getElementById('search-results').innerHTML += `
-                <div class="container card">
-                    <div class="card-body">
-                        <h4 class="card-title">${recipe.title}</h4>
-                        <p class="card-text">${recipe.summary}</p>
-                        <a href="#" class="btn btn-info">recipe link</a>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">posted by: <a class="blackText" href="...">${user.username}</a> on January 4, 2021</small>
-                    </div>
-                </div>
-                `
+    let searchQuery = document.getElementById('search-input').value;
+    let searchResultsContainer = document.getElementById('search-results');
+    searchResultsContainer.innerHTML = `<h3>Search Results</h3>`;
+    let selected = document.querySelector('.search-toggle .active input').value;
+    if (selected === 'title'){
+        searchRecipesByTitle(searchQuery).then((recipes) => {
+            recipes.forEach(recipe => {
+                getSpecificUser(recipe.created_by).then((user) => {
+                    let createdAt = new Date(recipe.createdAt);
+                    console.log(createdAt);
+                    searchResultsContainer.innerHTML += createSearchResults(recipe.title, recipe.summary, user.username, createdAt.toDateString());
+                })
+                
             })
-            
         })
-    })
+    } else if (selected === 'ingredients'){
+        searchRecipesByIngredient(searchQuery).then((recipes) => {
+            recipes.forEach(recipe => {
+                getSpecificUser(recipe.created_by).then((user) => {
+                    let createdAt = new Date(recipe.createdAt);
+                    console.log(createdAt);
+                    searchResultsContainer.innerHTML += createSearchResults(recipe.title, recipe.summary, user.username, createdAt.toDateString());
+                })
+                
+            })
+        })
+    }
+    
 });
+
+function createSearchResults(title, summary, username, date){
+    return `
+    <div class="container card">
+        <div class="card-body">
+            <h4 class="card-title">${title}</h4>
+            <p class="card-text">${summary}</p>
+            <a href="#" class="btn btn-info">recipe link</a>
+        </div>
+        <div class="card-footer">
+            <small>posted by: <a class="text-muted" href="...">${username}</a> on ${date}</small>
+        </div>
+    </div>
+    `
+}
