119bed11b35cfde9ae710f16e80c4094750e6a5f
rtanner0150 <71511920+tannertronic@users.noreply.github.com>
2021-01-21 10:57:06 -0600

    refactor search functions

--- public/js/search.js
+++ public/js/search.js
@@ -3,23 +3,17 @@ searchResultsContainer.innerHTML = `<h3>Search Results</h3>`;
 
 document.getElementById('search-submit').addEventListener('click', () => {
     let searchQuery = document.getElementById('search-input').value;
+    searchResultsContainer.innerHTML = `<h3>Search Results</h3>`;
     let selected = document.querySelector('.search-toggle .active input').value;
     let searchOn = selected;
     if (selected === 'ingredients'){
         searchOn = 'ingredients.name';
     }
-    searchRecipes(searchQuery, searchOn).then((recipes) => {
-        recipes.forEach(recipe => {
-            getSpecificUser(recipe.created_by).then((user) => {
-                let createdAt = new Date(recipe.createdAt);
-                searchResultsContainer.innerHTML += createSearchResults(recipe.title, recipe.summary, user.username, createdAt.toDateString());
-            })
-        })
-    })
+    searchRecipes(searchQuery, searchOn).then(recipes => populateSearchResults(recipes));
     
 });
 
-function createSearchResults(title, summary, username, date){
+function createSearchResultsHtml(title, summary, username, date){
     return `
     <div class="container card">
         <div class="card-body">
@@ -34,19 +28,21 @@ function createSearchResults(title, summary, username, date){
     `
 }
 
+function populateSearchResults(recipes){
+    recipes.forEach(recipe => {
+        getSpecificUser(recipe.created_by).then((user) => {
+            let createdAt = new Date(recipe.createdAt);
+            searchResultsContainer.innerHTML += createSearchResultsHtml(recipe.title, recipe.summary, user.username, createdAt.toDateString());
+        })
+    })
+}
+
 //pull up tags search on window load if tags query param exists
 window.addEventListener('load', event => {
     const urlParams = new URLSearchParams(window.location.search);
     let tagsQuery = urlParams.get('tags');
     console.log('tagsQuery: ' + tagsQuery);
     if (tagsQuery !== null){
-    searchRecipes(tagsQuery, 'tags').then((recipes) => {
-        recipes.forEach(recipe => {
-            getSpecificUser(recipe.created_by).then((user) => {
-                let createdAt = new Date(recipe.createdAt);
-                searchResultsContainer.innerHTML += createSearchResults(recipe.title, recipe.summary, user.username, createdAt.toDateString());
-            })
-        })
-    })
+    searchRecipes(tagsQuery, 'tags').then(recipes => populateSearchResults(recipes));
     }
 })
