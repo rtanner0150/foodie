691895acef850aeff2c8ded0920199b90e2f2087
rtanner0150 <71511920+tannertronic@users.noreply.github.com>
2021-02-04 14:37:19 -0600

    more updates

--- public/js/edit-recipe.js
+++ public/js/edit-recipe.js
@@ -13,7 +13,9 @@ getRecipeById(urlRecipeId).then((recipe) => {
     document.getElementById('ingredient' + i).value = recipe.ingredients[i].name;
     document.getElementById('amount' + i).value = recipe.ingredients[i].amount;
     document.getElementById('substitutions' + i).value = recipe.ingredients[i].substitutions;
-    addIngredientRow();
+    if (i < (recipe.ingredients.length - 1)){
+      addIngredientRow();
+    }
   }
   savedImagePath = recipe.picture;
 });
@@ -21,36 +23,3 @@ getRecipeById(urlRecipeId).then((recipe) => {
 document.getElementById('edit-recipe').addEventListener('click', () => {
   uploadRecipe(true);
 });
-
-
-
-//for later- when submitting edited recipe, if no file input, set as prev value
-// if (document.getElementById('image').files[0] === undefined){
-//   recipe.picture = recipe.picture
-// }
-
-// document.getElementById('create-recipe').addEventListener('click', () => {
-//   let title = document.getElementById('recipeTitle').value;
-//   let times = document.getElementById('times').value;
-//   let summary = document.getElementById('summary').value;
-//   let directions = document.querySelector('#editor .ql-editor').innerHTML;
-//   let tags = document.getElementById('tags').value;
-//   let ingredients = [];
-//   for (let i = 0; i < document.querySelectorAll('.ingredientRow').length; i++){
-//     let ingredient = {
-//       name: document.getElementById('ingredient' + i).value,
-//       amount: document.getElementById('amount' + i).value,
-//       substitutions: document.getElementById('substitutions' + i).value
-//     }
-//     ingredients.push(ingredient);
-//   }
-//   handleImageUpload(document.getElementById('image')).then((imagePath) => {
-//     console.log('imagePath: ' + imagePath);
-//     createRecipe(title, ingredients, times, directions, summary, imagePath, tags).then((result) => {
-//       console.log(result);
-//     }).catch((error) => {
-//       console.log(error);
-//     });
-//   });
-  
-// });
